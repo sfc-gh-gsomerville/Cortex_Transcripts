746ffa6231a91da0701a06afef0f5808b7ed1fb7
gsomervi930 <111148835+gsomervi930@users.noreply.github.com>
2025-03-29 23:25:07 -0400

    Update config.py

--- Python_Analysis/config.py
+++ Python_Analysis/config.py
@@ -2,7 +2,7 @@
 CONNECTION_PARAMETERS = {
     "account": "CGKCUIL-MUB55006",  # e.g., "xy12345.us-east-1" or "xy12345.region.aws" 
     "user": "gsomerville",
-    "password": "rSrga7jVvJBq7EY",  # Consider using environment variables for sensitive info
+    "password": "<password>",  # Consider using environment variables for sensitive info
     "role": "ACCOUNTADMIN",          # e.g., "ACCOUNTADMIN"
     "warehouse": "COMPUTE_WH",  # Updated to use an existing warehouse
     "database": "CORTEX",
